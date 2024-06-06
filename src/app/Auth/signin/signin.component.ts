196519dd04f834997b7ae627a037e8bf77a61774
Ludovic <ludoverhenne@hotmail.com>
2024-06-06 21:51:39 +0200

    Développement de signup et signin.

--- src/app/Auth/signin/signin.component.ts
+++ src/app/Auth/signin/signin.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-signin',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./signin.component.css']
 })
 export class SigninComponent {
+  isLoading = false;
 
+  onLogin(form: NgForm) {
+    
+  }
 }
