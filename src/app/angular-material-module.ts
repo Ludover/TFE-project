d0e454036882227e47b37a21743bd0fb93ef9d9d
Ludovic <ludoverhenne@hotmail.com>
2024-08-22 17:00:35 +0200

    Réorganisation des modules.

---
+++ src/app/angular-material-module.ts
@@ -0,0 +1,39 @@
+import { NgModule  } from "@angular/core";
+
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatListModule } from '@angular/material/list';
+import { MatSelectModule } from '@angular/material/select';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatBadgeModule } from '@angular/material/badge';
+
+@NgModule({   
+    exports: [
+        MatInputModule,
+        MatFormFieldModule,
+        MatButtonModule,
+        MatCardModule,
+        MatToolbarModule,
+        MatExpansionModule,
+        MatProgressSpinnerModule,
+        MatIconModule,
+        MatSnackBarModule,
+        MatMenuModule,
+        MatListModule,
+        MatSelectModule,
+        MatDialogModule,
+        MatPaginatorModule,
+        MatBadgeModule
+    ]
+})
+
+export class AngularMaterialModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,31 +6,17 @@ import {
   HttpClient,
   HttpClientModule,
 } from '@angular/common/http';
-import { MatInputModule } from '@angular/material/input';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatButtonModule } from '@angular/material/button';
-import { MatCardModule } from '@angular/material/card';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatExpansionModule } from '@angular/material/expansion';
-import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
-import { MatIconModule } from '@angular/material/icon';
-import { MatSnackBarModule } from '@angular/material/snack-bar';
-import { MatMenuModule } from '@angular/material/menu';
-import { MatListModule } from '@angular/material/list';
-import { MatSelectModule } from '@angular/material/select';
-import { MatDialogModule } from '@angular/material/dialog';
-import { MatPaginatorModule } from '@angular/material/paginator';
-import { MatBadgeModule } from '@angular/material/badge';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './header/header.component';
 import { MovieListComponent } from './movies/movie-list/movie-list.component';
 import { AppRoutingModule } from './app-routing.module';
+import { AuthInterceptor } from './auth/auth-interceptor';
+import { ErrorInterceptor } from './error-interceptor';
 import { SignupComponent } from './auth/signup/signup.component';
 import { SigninComponent } from './auth/signin/signin.component';
 import { SearchComponent } from './movies/search/search.component';
-import { AuthInterceptor } from './auth/auth-interceptor';
 import { MovieListDoneComponent } from './movies/movie-list-done/movie-list-done.component';
 import { AddFriendComponent } from './friends/add friend/add-friend.component';
 import { FriendListComponent } from './friends/list/friend-list.component';
@@ -41,7 +27,8 @@ import { MovieListRecommendedComponent } from './movies/movie-list-recommended/m
 import { MovieDetailsDialogComponent } from './movies/movie-details-dialog/movie-details-dialog.component';
 import { DatePipe, registerLocaleData } from '@angular/common';
 import localeFr from '@angular/common/locales/fr';
-import { ErrorInterceptor } from './error-interceptor';
+
+import { AngularMaterialModule } from './angular-material-module';
 
 registerLocaleData(localeFr);
 
@@ -68,21 +55,7 @@ registerLocaleData(localeFr);
     FormsModule,
     BrowserAnimationsModule,
     HttpClientModule,
-    MatInputModule,
-    MatFormFieldModule,
-    MatButtonModule,
-    MatCardModule,
-    MatToolbarModule,
-    MatExpansionModule,
-    MatProgressSpinnerModule,
-    MatIconModule,
-    MatSnackBarModule,
-    MatMenuModule,
-    MatListModule,
-    MatSelectModule,
-    MatDialogModule,
-    MatPaginatorModule,
-    MatBadgeModule
+    AngularMaterialModule
   ],
   providers: [
     { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
